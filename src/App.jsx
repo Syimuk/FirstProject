9783ffbead5d8917cd463ce11c10546483569b1b
Syimuk S. <raftasher2@gmail.com>
2024-12-10 12:34:17 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-import React from "react";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <div>
-      <h1>Happy borthday to You, Aitosh</h1>
-      <p>
-        Hy что, моя хорошая, вот и настал тот день, в который ты когда-то
-        появилась на свет. Пусть праздник будет не только сегодня, но и каждый
-        день. Радуйся, люби и будь любима, наслаждайся каждым днем, каждым
-        моментом. Будь самой счастливой на этой планете. Пусть все боли, обиды,
-        негатив обходят тебя стороной.C днем рождения, подруга!
-      </p>
-      <iframe
-        width="1522"
-        height="688"
-        src="https://www.youtube.com/embed/n3qw7lZzbkM?si=rY9cHFSroDHxrrze"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-      ></iframe>
-
-      <p>Bye Bye!</p>
+      <Header />
     </div>
   );
 }
